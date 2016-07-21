66916a5c1f88bddcdfda774c9b0281e74d813fca
Cihangir SAVAS <cihangir@koding.com>
2016-07-21 17:56:16 +0300

    data: source env.sh with $(dirname $0)

--- data/install_app.sh
+++ data/install_app.sh
@@ -1,14 +1,16 @@
-#!/bin/bash
+#!/bin/bash -x
 
 set -o errexit
 
-YUM="yum --assumeyes"
-
-. ./env.sh
+#  make relative paths work.
+cd $(dirname $0)/..
 
 source /etc/profile
-
+source $(dirname $0)/env.sh
 export PATH=${JAVA_HOME}/bin:${PATH}
+
+YUM="yum --assumeyes"
+
 
 
 # INSTALL MAVEN
@@ -36,33 +38,37 @@ useradd -g $ZK_USER -M -r $ZK_USER
 # set permissions for zookeeper
 mkdir -p /var/{lib,log}/zookeeper
 chown -R $ZK_USER:$ZK_USER /var/{lib,log}/zookeeper
+
+mkdir -p /var/{lib,log}/exhibitor
+chown -R $ZK_USER:$ZK_USER /var/{lib,log}/exhibitor
+
 chown -R $ZK_USER:$ZK_USER ${ZK_HOME}
 chown -R $ZK_USER:$ZK_USER ${ZK_DATA_DIR}
 chown -R $ZK_USER:$ZK_USER ${ZK_LOG_DIR}
 
 
 cat <<- EOF > $EXBT_HOME/defaults.conf
-    zookeeper-data-directory=$ZK_DATA_DIR
-    zookeeper-install-directory=$ZK_HOME
-    zookeeper-log-directory=$ZK_LOG_DIR
-    log-index-directory=$ZK_LOG_DIR
-    cleanup-period-ms=300000
-    check-ms=30000
-    backup-period-ms=600000
-    client-port=2181
-    cleanup-max-files=20
-    backup-max-store-ms=21600000
-    connect-port=2888
-    observer-threshold=0
-    election-port=3888
-    zoo-cfg-extra=tickTime\=2000&initLimit\=10&syncLimit\=5&quorumListenOnAllIPs\=true
-    auto-manage-instances-settling-period-ms=0
-    auto-manage-instances=1
-    auto-manage-instances-fixed-ensemble-size=$ZK_ENSEMBLE_SIZE
-    backup-extra=throttle\=&bucket-name\=$EXBT_S3_BUCKET&key-prefix\=$EXBT_S3_BACKUP_PREFIX&max-retries\=4&retry-sleep-ms\=30000
+zookeeper-data-directory=$ZK_DATA_DIR
+zookeeper-install-directory=$ZK_HOME
+zookeeper-log-directory=$ZK_LOG_DIR
+log-index-directory=$ZK_LOG_DIR
+cleanup-period-ms=300000
+check-ms=30000
+backup-period-ms=600000
+client-port=2181
+cleanup-max-files=20
+backup-max-store-ms=21600000
+connect-port=2888
+observer-threshold=0
+election-port=3888
+zoo-cfg-extra=tickTime\=2000&initLimit\=10&syncLimit\=5&quorumListenOnAllIPs\=true
+auto-manage-instances-settling-period-ms=0
+auto-manage-instances=1
+auto-manage-instances-fixed-ensemble-size=$ZK_ENSEMBLE_SIZE
+backup-extra=throttle\=&bucket-name\=$EXBT_S3_BUCKET&key-prefix\=$EXBT_S3_BACKUP_PREFIX&max-retries\=4&retry-sleep-ms\=30000
 EOF
 
 cat <<- EOF > $EXBT_HOME/credentials.properties
-    com.netflix.exhibitor.s3.access-key-id=${AWS_ACCESS_KEY}
-    com.netflix.exhibitor.s3.access-secret-key=${AWS_SECRET_KEY}
+com.netflix.exhibitor.s3.access-key-id=${AWS_ACCESS_KEY}
+com.netflix.exhibitor.s3.access-secret-key=${AWS_SECRET_KEY}
 EOF
