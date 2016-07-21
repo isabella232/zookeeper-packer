83dd3cc5119137655bb7857420491d1d5b69598b
Cihangir SAVAS <cihangir@koding.com>
2016-07-21 15:52:39 +0300

    data: add required env variables

--- data/env.sh
+++ data/env.sh
@@ -5,12 +5,13 @@ export HOST_IP=${HOST_IP:-`curl http://169.254.169.254/latest/meta-data/local-ip
 
 # SUPERVISORD CONFIGS
 export SUPERVISORD_RUNDIR=${SUPERVISORD_RUNDIR:-/var/run}
-export SUPERVISORD_LOGDIR=${SUPERVISORD_LOGDIR:-/var/log/druid}
+export SUPERVISORD_LOGDIR=${SUPERVISORD_LOGDIR:-/var/log/zookeeper}
 export SUPERVISORD_MINFDS=${SUPERVISORD_MINFDS:-10000}
 export SUPERVISORD_MINPROCS=${SUPERVISORD_MINPROCS:-200}
 export SUPERVISORD_UNIX_HTTP_SERVER_FILE=${SUPERVISORD_UNIX_HTTP_SERVER_FILE:-/var/run/supervisor.sock}
 
 # AWS CONFIG
+export AWS_REGION=${AWS_REGION:-AWS_REGION}
 export AWS_ACCESS_KEY=${AWS_ACCESS_KEY:-AWS_ACCESS_KEY}
 export AWS_SECRET_KEY=${AWS_SECRET_KEY:-AWS_SECRET_KEY}
 
@@ -20,3 +21,23 @@ export JAVA_IO_TMP_DIR=${JAVA_IO_TMP_DIR:-/tmp}
 # DRUID CONFIG
 export ROOT_DIR=${ROOT_DIR:-/opt/zookeeper}
 
+export JAVA_HOME="/usr/java/latest"
+
+export ZK_HOME=${ZK_HOME:-/opt/zookeeper}
+export ZK_VERSION=${ZK_VERSION:-3.4.8}
+export ZK_RELEASE="http://www.apache.org/dist/zookeeper/zookeeper-$ZK_VERSION/zookeeper-$ZK_VERSION.tar.gz"
+export ZK_DATA_DIR=${ZK_DATA_DIR:-$ZK_HOME/snapshots}
+export ZK_LOG_DIR=${ZK_LOG_DIR:-$ZK_HOME/transactions}
+export ZK_USER=${ZK_USER:-zookeeper}
+export ZK_ENSEMBLE_SIZE=${ZK_ENSEMBLE_SIZE:-0}
+
+
+export EXBT_HOME=${EXBT_HOME:-/opt/exhibitor}
+export EXHIBITOR_POM="https://raw.githubusercontent.com/Netflix/exhibitor/d911a16d704bbe790d84bbacc655ef050c1f5806/exhibitor-standalone/src/main/resources/buildscripts/standalone/maven/pom.xml"
+export EXBT_S3_BUCKET=${EXBT_S3_BUCKET:-kodingdev-exhitbitor-test-delete-if-you-still-see-this:zookeeper}
+export EXBT_S3_BACKUP=${EXBT_S3_BACKUP:-true}
+export EXBT_S3_BACKUP_PREFIX=${EXBT_S3_BACKUP_PREFIX:-zookeeper-backup}
+export EXBT_PORT=${EXBT_PORT:-8181}
+
+export MAVEN_HOME=${MAVEN_HOME:-/usr/share/maven}
+export MAVEN_VERSION=${MAVEN_VERSION:-3.3.3}
